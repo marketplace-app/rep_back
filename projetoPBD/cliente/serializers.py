225d8c4882be9aaaad9627a80e3bdb42a583e5a0
João Vitor <joaovitor.alcantara015@gmail.com>
2024-11-18 00:40:21 -0300

    Delete projetoPBD directory

--- projetoPBD/cliente/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from cliente.models import Cliente
-
-class ClienteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cliente
-        fields = ["id", "nome", "email", "telefone", "data_cadastro", "ativo"]
